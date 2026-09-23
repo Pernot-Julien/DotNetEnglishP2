b1b3b27008197abd8aa4da5f178b5afdeb5ae7d2
Julien <roulio555@gmail.com>
2026-09-23 13:31:01 +0200

    Ajout des fichiers de traduction espagnole

--- P2FixAnAppDotNetCode/Models/Order.cs
+++ P2FixAnAppDotNetCode/Models/Order.cs
@@ -20,7 +20,8 @@ namespace P2FixAnAppDotNetCode.Models
 
         [Required(ErrorMessage = "ErrorMissingCity")]
         public string City { get; set; }
-
+        
+        [Required(ErrorMessage = "ErrorMissingZip")]
         public string Zip { get; set; }
 
         [Required(ErrorMessage = "ErrorMissingCountry")]
